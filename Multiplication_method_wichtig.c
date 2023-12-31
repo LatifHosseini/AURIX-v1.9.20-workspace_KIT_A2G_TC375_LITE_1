bc5e7fdac478c717b992962cffad1c1dc6691bb0
LatifHosseini <113984497+LatifHosseini@users.noreply.github.com>
2023-12-31 23:16:07 +0100

    multiplication method

---
+++ Multiplication_method_wichtig.c
@@ -0,0 +1,13 @@
+
+
+x << k == x multiplied by 2 to the power of k
+x >> k == x divided by 2 to the power of k
+
+You can use these shifts to do any multiplication operation. For example:
+
+x * 14 == x * 16 - x * 2 == (x << 4) - (x << 1)
+x * 12 == x * 8 + x * 4 == (x << 3) + (x << 2)
+
+To divide a number by a non-power of two, I'm not aware of any easy way, 
+unless you want to implement some low-level logic, use other binary operations 
+and use some form of iteration.
